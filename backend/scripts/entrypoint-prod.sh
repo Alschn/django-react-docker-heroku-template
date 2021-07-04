b09369b20edd11b761c06e3661761ac581a483a5
Alschn <adam.lisichin@gmail.com>
2021-07-04 19:53:51 +0200

    Update entrypoint script

--- backend/scripts/entrypoint-prod.sh
+++ backend/scripts/entrypoint-prod.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-python ./backend/manage.py makemigrations --no-input
-python ./backend/manage.py migrate --no-input
+python3 backend/manage.py makemigrations --no-input
+python3 backend/manage.py migrate --no-input
 
+python3 backend/manage.py runserver 0.0.0.0:$PORT
